a2cb7e318759b7ed190f30fabd37be379257fe55
Niclas <niclas.gleesborg@gmail.com>
2024-12-11 22:31:35 +0100

    Initial commit

--- apps/mf-visualization/src/app/app.routes.ts
+++ apps/mf-visualization/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: "shared-ui",
+    loadComponent: () =>
+      import("@niclas/ui").then((m) => m.UiComponent )
+  }
+];
