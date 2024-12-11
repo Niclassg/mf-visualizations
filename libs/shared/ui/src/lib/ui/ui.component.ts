2c1ac739d1f73d2231a805ec61c2b65d0c2eaebd
Niclas <niclas.gleesborg@gmail.com>
2024-12-11 23:06:28 +0100

    Add Angular Material integration and update global styles
    Integrated Angular Material with the project, adding necessary dependencies, themes, and components such as `mat-slide-toggle`. Updated global styles to include Roboto font and Material Icons, while removing unused dependencies. Enhanced the UI foundation for better styling and functionality.

--- libs/shared/ui/src/lib/ui/ui.component.ts
+++ libs/shared/ui/src/lib/ui/ui.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatSlideToggle } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'lib-ui',
-  imports: [CommonModule],
+  imports: [CommonModule, MatSlideToggle],
   templateUrl: './ui.component.html',
   styleUrl: './ui.component.css',
 })
